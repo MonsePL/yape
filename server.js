4e06b74839f85dd92db6cdb14c5d452a8dba84b4
Monserrat Pineda <monserrat.pineda.luna@gmail.com>
2017-07-18 13:20:33 -0500

    carrousel

--- server.js
+++ server.js
@@ -18,6 +18,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static('public'));
 app.use("/static", express.static(__dirname + '/node_modules'));
+app.use("/static", express.static(__dirname + '/assets'));
 app.use(morgan(format));
 
 app.get("/",function(req,res){
