e8da824ed3ddcf37883a85b93861c72bda5977e6
Monserrat Pineda <monserrat.pineda.luna@gmail.com>
2017-07-21 10:54:47 -0500

    ultima pantalla y empezar a utilizar appi

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,22 @@
+$(document).ready(function() {
+  $('.carousel').carousel();
+});
 
-    $(document).ready(function(){
-      $('.carousel').carousel();
-    });
+$('.carousel.carousel-slider').carousel({
+  fullWidth: true
+});
 
-    $('.carousel.carousel-slider').carousel({fullWidth: true});
+
+function postJSON(url) {
+  return new Promise(function(resolve, reject) {
+    var ajax = new XMLHttpRequest();
+    ajax.open("POST", url);
+    ajax.send();
+    ajax.onreadystatechange = function() {
+      if (ajax.readyState == 4) {
+        resolve(JSON.parse(ajax.responseText))
+      }
+
+    }
+  })
+}
